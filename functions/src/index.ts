20d0eeb243cfe35f7e5687f9a5e7c52bd9b2052c
onekingdev <onekingdev@gmail.com>
2020-12-13 15:57:14 -0600

    sql

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import {connect} from './config';
+import {Users} from './entities/Users';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +8,12 @@
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+export const getUsers = functions.https.onRequest(async (request, response) => {
+    const connection = await connect();
+    const repo = connection.getRepository(Users);
+
+    const all = await repo.find();
+
+    response.send(all);
+});
