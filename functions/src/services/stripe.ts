8e019fedd2666ddbd55a88c0cb53e58f013ebcaa
onekingdev <onekingdev@gmail.com>
2021-08-17 18:18:37 -0500

    change price keys

--- functions/src/services/stripe.ts
+++ functions/src/services/stripe.ts
@@ -1,39 +1,27 @@
-require('dotenv').config();
+require("dotenv").config();
 
 export const getStripeKey = {
-    stripe_secret: (env: string) => {
-        if (env === 'production') return String(process.env.STRIPE_PRODUCTION_KEY);
-        return String(process.env.STRIPE_DEVELOPMENT_KEY);
-
-    },
-    subscription_price: (env: string, type: string) => {
-       if (env === 'production') 
-       {
-            if(type === "CL AI")
-            {
-                return String(process.env.PROD_PRICE_ONE_KEY);
-            }
-            else
-            {
-                return String(process.env.PROD_PRICE_TWO_KEY);
-            }
-       }
-       else
-       {
-            if(type === "CL AI")
-            {
-                return String(process.env.DEV_PRICE_ONE_KEY);
-            }
-            else
-            {
-                return String(process.env.DEV_PRICE_TWO_KEY);
-            }
-       }
-        
-
-    },
-    hook_secret: (env: string) => {
-        if (env === 'production') return 'whsec_uhC7g1hinSwzmmyBCKbWTsBLBRemkzBI'
-        return 'whsec_PN7zX0x2NB093oANjDH9MgifE6ApxYqW'
-    },
-}
+  stripe_secret: (env: string) => {
+    if (env === "production") return String(process.env.STRIPE_PRODUCTION_KEY);
+    return String(process.env.STRIPE_DEVELOPMENT_KEY);
+  },
+  subscription_price: (env: string, type: string) => {
+    if (env === "production") {
+      if (type === "CL AI") {
+        return String("price_1JK37pAT9ya87fpTjdhp4YMc");
+      } else {
+        return String("price_1JK37pAT9ya87fpT2bvLdmi8");
+      }
+    } else {
+      if (type === "CL AI") {
+        return String(process.env.DEV_PRICE_ONE_KEY);
+      } else {
+        return String(process.env.DEV_PRICE_TWO_KEY);
+      }
+    }
+  },
+  hook_secret: (env: string) => {
+    if (env === "production") return "whsec_uhC7g1hinSwzmmyBCKbWTsBLBRemkzBI";
+    return "whsec_PN7zX0x2NB093oANjDH9MgifE6ApxYqW";
+  },
+};
