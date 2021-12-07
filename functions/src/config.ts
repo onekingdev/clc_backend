d39a557f2b6cce5d92354c96e60de0e034e417ed
onekingdev <onekingdev@gmail.com>
2021-12-08 01:41:37 +0900

    firebase project name changed, mysql address changed

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -4,13 +4,38 @@ import 'reflect-metadata';
 export const prod = process.env.NODE_ENV === 'production';
 export const stripe_env = 'production';
 
+// export const config: ConnectionOptions = {
+//     name: 'clc',
+//     type: 'mysql',
+//     host:'35.238.13.230',
+//     port: 3306,
+//     username: 'clcdevelopers',
+//     password: 'Socrat3s!123',
+//     database: 'clc',
+//     synchronize: true,
+//     logging: false,
+//     entities: [
+//         'lib/entities/*.js'
+//     ],
+
+//     // Production Mode
+//     ...(prod && {
+//         database: 'clc',
+//         logging: false,
+//         // synchronize: false,
+//         extra: {
+//             socketPath: '/cloudsql/chipleadercoaching-webapp:us-central1:clc'
+//         }
+//     }),
+// }
+
 export const config: ConnectionOptions = {
     name: 'clc',
     type: 'mysql',
-    host:'35.238.13.230',
+    host:'34.66.49.142',
     port: 3306,
-    username: 'clcdevelopers',
-    password: 'Socrat3s!123',
+    username: 'devs',
+    password: 'devs1234',
     database: 'clc',
     synchronize: true,
     logging: false,
