7c26c0563d43e4bd12dc49492c5a56eb4ac1b548
onekingdev <onekingdev@gmail.com>
2021-12-09 01:47:23 +0900

    changed for upgrade card

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -4,38 +4,13 @@ import 'reflect-metadata';
 export const prod = process.env.NODE_ENV === 'production';
 export const stripe_env = 'production';
 
-// export const config: ConnectionOptions = {
-//     name: 'clc',
-//     type: 'mysql',
-//     host:'35.238.13.230',
-//     port: 3306,
-//     username: 'clcdevelopers',
-//     password: 'Socrat3s!123',
-//     database: 'clc',
-//     synchronize: true,
-//     logging: false,
-//     entities: [
-//         'lib/entities/*.js'
-//     ],
-
-//     // Production Mode
-//     ...(prod && {
-//         database: 'clc',
-//         logging: false,
-//         // synchronize: false,
-//         extra: {
-//             socketPath: '/cloudsql/chipleadercoaching-webapp:us-central1:clc'
-//         }
-//     }),
-// }
-
 export const config: ConnectionOptions = {
     name: 'clc',
     type: 'mysql',
-    host:'34.66.49.142',
+    host:'35.238.13.230',
     port: 3306,
-    username: 'devs',
-    password: 'devs1234',
+    username: 'clcdevelopers',
+    password: 'Socrat3s!123',
     database: 'clc',
     synchronize: true,
     logging: false,
@@ -53,6 +28,31 @@ export const config: ConnectionOptions = {
         }
     }),
 }
+
+// export const config: ConnectionOptions = {
+//     name: 'clc',
+//     type: 'mysql',
+//     host:'34.66.49.142',
+//     port: 3306,
+//     username: 'devs',
+//     password: 'devs1234',
+//     database: 'clc',
+//     synchronize: true,
+//     logging: false,
+//     entities: [
+//         'lib/entities/*.js'
+//     ],
+
+//     // Production Mode
+//     ...(prod && {
+//         database: 'clc',
+//         logging: false,
+//         // synchronize: false,
+//         extra: {
+//             socketPath: '/cloudsql/chipleadercoaching-webapp:us-central1:clc'
+//         }
+//     }),
+// }
 
 export const connect = async () => {
     let connection: Connection;
